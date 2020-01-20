3acdda964932db78d7117a78fd4c6681525410ae
Leonardo Fabián Hernández Peña <IngenieroLeonardo@outlook.com>
2020-01-19 20:26:10 -0500

    Agregar archivos de proyecto.

---
+++ APINETCORE/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using APINETCORE.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINETCORE.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
